169445f6e0e686bbd246403b0307e3974a1f3c46
johansabino <jasc9494@gmail.com>
2026-02-13 18:23:38 -0500

    fix home blade php

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,10 +2,10 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'toggleColour text-white inline-block py-2 px-4 font-bold no-underline'
-            : 'toggleColour text-white inline-block py-2 px-4 no-underline hover:underline';
+  ? 'toggleColour inline-block py-2 px-4 font-bold no-underline text-gray-800 lg:text-white'
+  : 'toggleColour inline-block py-2 px-4 no-underline hover:underline text-gray-800 lg:text-white lg:hover:text-gray-200';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
+  {{ $slot }}
 </a>
